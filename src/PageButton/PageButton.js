750991eae10e67727de9fd94deecf31a8f7a54a0
Arman <aisenov1995@gmail.com>
2021-10-26 17:28:24 +0500

    Создана логика рсчета налогового вычета!

--- src/PageButton/PageButton.js
+++ src/PageButton/PageButton.js
@@ -2,10 +2,21 @@ import React, {useState} from "react";
 import styles from './PageButton.module.css'
 import ModalWindow from "../ModalWindow/ModalWindow";
 
+
 const PageButton = () => {
 
     const [isModalActive, setIsModalActive] = useState(false)
     const [isVisibility, setIsVisibility] = useState(true)
+    const [inputValue, setInputValue] = useState('')
+
+    const handleOnChangeInputValue = event => setInputValue(event.target.value)
+
+    let sum = parseInt(inputValue * 12 * 13/100)
+
+    const handleOnClickCount = () => {
+        setIsVisibility(false)
+    }
+
 
     return(
         <div className={styles.container__background}>
@@ -25,32 +36,35 @@ const PageButton = () => {
                 <div className={styles.container__body}>
                     <div className={styles.container__context_func}>
                         <h4 className={styles.info__name_sum}>Ваша зарплата в месяц</h4>
-                        <input className={styles.info__input} type="number" placeholder="Введите данные"/>
+                        <input className={styles.info__input} type="number" placeholder="Введите данные"
+                               onChange={handleOnChangeInputValue}
+                               value={inputValue}
+                        />
                         <button className={styles.button__calc}
-                                onClick={() => setIsVisibility(isVisibility ? false : true)}
+                                onClick={handleOnClickCount}
                         >Рассчитать</button>
                         <div className={styles.container__context_result} hidden={isVisibility}>
                             <h5 className={styles.info__name_result}>Итого можете внести <br/>в качестве досрочных:</h5>
                             <ul className={styles.unordered__list}>
                                 <li className={styles.unordered__list_point}>
                                     <input className={styles.unordered__list_point_checkbox_first} type="checkbox" id="point_first" />
-                                    <label htmlFor="point_first">78 000 рублей <span className={styles.unordered__list_point_year}>в 1-ый год</span></label>
+                                    <label htmlFor="point_first">{sum} рублей <span className={styles.unordered__list_point_year}>в 1-ый год</span></label>
                                 </li>
                                 <li className={styles.unordered__list_point}>
                                     <input className={styles.unordered__list_point_checkbox_second} type="checkbox" id="point_second" />
-                                    <label htmlFor="point_second">78 000 рублей <span className={styles.unordered__list_point_year}>в 2-ой год</span></label>
+                                    <label htmlFor="point_second">{sum} рублей <span className={styles.unordered__list_point_year}>в 2-ой год</span></label>
                                 </li>
                                 <li className={styles.unordered__list_point}>
                                     <input className={styles.unordered__list_point_checkbox_third} type="checkbox" id="point_third" />
-                                    <label htmlFor="point_third">78 000 рублей <span className={styles.unordered__list_point_year}>в 3-ий год</span></label>
+                                    <label htmlFor="point_third">{sum} рублей <span className={styles.unordered__list_point_year}>в 3-ий год</span></label>
                                 </li>
                                 <li className={styles.unordered__list_point}>
                                     <input className={styles.unordered__list_point_checkbox_fourth} type="checkbox" id="point_fourth" />
-                                    <label htmlFor="point_fourth">78 000 рублей <span className={styles.unordered__list_point_year}>в 4-ый год</span></label>
+                                    <label htmlFor="point_fourth">{sum} рублей <span className={styles.unordered__list_point_year}>в 4-ый год</span></label>
                                 </li>
                                 <li className={styles.unordered__list_point}>
                                     <input className={styles.unordered__list_point_checkbox_five} type="checkbox" id="point_fifth"/>
-                                    <label htmlFor="point_fifth">26 000 рублей <span className={styles.unordered__list_point_year}>в 5-ый год</span></label>
+                                    <label htmlFor="point_fifth">{sum} рублей <span className={styles.unordered__list_point_year}>в 5-ый год</span></label>
                                 </li>
                             </ul>
                         </div>
