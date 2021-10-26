20cd8f0958506fc503fea61834fe630b93c1ba77
Arman <aisenov1995@gmail.com>
2021-10-26 16:31:03 +0500

    Удалён лишний код!

--- src/ModalWindow/ModalWindow.js
+++ src/ModalWindow/ModalWindow.js
@@ -2,14 +2,12 @@ import React from "react";
 
 import './ModalWindow.css'
 
-const ModalWindow = ({isModalActive, isVisibility, children}) => {
+const ModalWindow = ({isModalActive, children}) => {
 
     return(
         <div className={isModalActive ? 'container__background_module active' : 'container__background_module' }
-             // style={{minWidth:'768px'} ? {minWidth: `100%`, minHeight: `100%`} : }
         >
             <div className={isModalActive ? 'container__content_module active': 'container__content_module'}
-                 // style={!isVisibility ? {maxHeight:`760px`} : {maxHeight:`476px`}}
             >
 
                 {children}
